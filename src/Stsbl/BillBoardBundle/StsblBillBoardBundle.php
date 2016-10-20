9307cda05b0fbb5a79a106fd72611c0f9305e739
Felix Jacobi <felix.jacobi@stsbl.de>
2016-10-20 06:17:48 +0200

    Coding standard improvements.

--- src/Stsbl/BillBoardBundle/StsblBillBoardBundle.php
+++ src/Stsbl/BillBoardBundle/StsblBillBoardBundle.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * @author Felix Jacobi <felix.jacobi@stsbl.de>
  * @license http://gnu.org/licenses/gpl-3.0 GNU General Public License
  */
-
 class StsblBillBoardBundle extends Bundle implements AutoloadRoutingBundleInterface
 {
     public function getContainerExtension()
