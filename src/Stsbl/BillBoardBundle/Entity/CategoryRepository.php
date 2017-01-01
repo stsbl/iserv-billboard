4d2962758b7f82f48c32cc2a86fe785426758535
Felix Jacobi <felix.jacobi@stsbl.de>
2017-01-01 10:03:34 +0100

    Updated copyright and headers.
    Note: License has changed from GPL3.0+ to MIT.

--- src/Stsbl/BillBoardBundle/Entity/CategoryRepository.php
+++ src/Stsbl/BillBoardBundle/Entity/CategoryRepository.php
@@ -5,9 +5,33 @@ namespace Stsbl\BillBoardBundle\Entity;
 use Doctrine\ORM\NoResultException;
 use IServ\CrudBundle\Doctrine\ORM\EntitySpecificationRepository;
 
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Felix Jacobi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
  * @author Felix Jacobi <felix.jacobi@stsbl.de>
- * @license http://gnu.org/licenses/gpl-3.0 GNU Lesser General Public License
+ * @license MIT license <https://mit.otg/licenses/MIT>
  */
 class CategoryRepository extends EntitySpecificationRepository
 {
